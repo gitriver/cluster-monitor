300d3b85f63b6938374c000fa39464c3511d6f15
fding123 <455726253@qq.com>
2016-03-11 10:41:29 +0800

    增加hdfs监控

---
+++ src/main/java/cn/com/systex/cluster/monitor/HdfsMonitor.java
@@ -0,0 +1,133 @@
+package cn.com.systex.cluster.monitor;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.PrivilegedExceptionAction;
+import java.util.Properties;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+import org.apache.hadoop.hdfs.protocol.HdfsConstants;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import com.google.common.base.Strings;
+
+/**
+ * 获取datanode信息 需要先设置dfs.permissions
+ * 或者设置用户 dfs.permissions.supergroup
+ * @author ding
+ *
+ */
+public class HdfsMonitor {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(HdfsMonitor.class);
+	public static int BUFFER_SIZE;
+	public static FileSystem fs;
+	public static Configuration conf;
+
+	public static void main(String[] args) throws Exception {
+		HdfsMonitor
+				.init("D:/Program/workspace/stp-parent/stp-util/src/main/resources/hadoop.properties");
+
+	}
+
+	public static void printDatanodeInfo() {
+		DistributedFileSystem hdfs = (DistributedFileSystem) fs;
+		DatanodeInfo[] live = hdfs.getDataNodeStats();
+		for (DatanodeInfo datanodeInfo : live) {
+			System.out.println(datanodeInfo.getDatanodeReport());
+		}
+	}
+	
+
+	public static void printFsStatus() throws IOException {
+		DistributedFileSystem hdfs = (DistributedFileSystem) fs;
+		FsStatus ds = hdfs.getStatus();
+		long capacity = ds.getCapacity();
+		long used = ds.getUsed();
+		long remaining = ds.getRemaining();
+		long presentCapacity = used + remaining;
+		boolean mode = hdfs
+				.setSafeMode(HdfsConstants.SafeModeAction.SAFEMODE_GET);
+		if (mode) {
+			System.out.println("Safe mode is ON");
+		}
+		System.out.println("Configured Capacity: " + capacity + " ("
+				+ StringUtils.byteDesc(capacity) + ")");
+		System.out.println("Present Capacity: " + presentCapacity + " ("
+				+ StringUtils.byteDesc(presentCapacity) + ")");
+		System.out.println("DFS Remaining: " + remaining + " ("
+				+ StringUtils.byteDesc(remaining) + ")");
+		System.out.println("DFS Used: " + used + " ("
+				+ StringUtils.byteDesc(used) + ")");
+		System.out
+				.println("DFS Used%: "
+						+ StringUtils.formatPercent(used
+								/ (double) presentCapacity, 2));
+
+		System.out.println("Under replicated blocks: "
+				+ hdfs.getUnderReplicatedBlocksCount());
+		System.out.println("Blocks with corrupt replicas: "
+				+ hdfs.getCorruptBlocksCount());
+		System.out.println("Missing blocks: " + hdfs.getMissingBlocksCount());
+	}
+	
+	
+
+	// 初始化环境
+	public static void init(String file) {
+		Properties props = getProperties(file);
+		BUFFER_SIZE = Integer.parseInt(props.getProperty("file.buffer.size"));
+
+		conf = new Configuration();
+		conf.addResource(new Path(props.getProperty("hdfs.conf.file")));
+		conf.set("fs.defaultFS", props.getProperty("fs.defaultFS"));
+		try {
+			String ugi = props.getProperty("fs.ugi");
+			if (Strings.isNullOrEmpty(ugi))
+				fs = FileSystem.get(conf);
+			else {
+				UserGroupInformation.createProxyUser(ugi,
+						UserGroupInformation.getLoginUser()).doAs(
+						new PrivilegedExceptionAction<Void>() {
+							@Override
+							public Void run() throws Exception {
+								fs = FileSystem.get(conf);
+								return null;
+							}
+
+						});
+			}
+		} catch (Exception e) {
+			LOG.error("初始化FileSytem对象异常: ", e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+	public static Properties getProperties(String propFile) {
+		Properties props = new Properties();
+		try {
+			InputStream stream = new FileInputStream(propFile);
+			props.load(stream);
+			stream.close();
+		} catch (FileNotFoundException e) {
+			LOG.error("解析数据库配置文件:No such file " + propFile);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			LOG.error("解析数据库配置文件:" + e1.getMessage() + "," + propFile);
+		}
+		return props;
+	}
+}
---
+++ src/main/java/cn/com/systex/cluster/monitor/Test.java
@@ -0,0 +1,67 @@
+package cn.com.systex.cluster.monitor;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.FsStatus;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+
+/**
+ * 获取datanode信息 需要先设置dfs.permissions
+ * 
+ * @author ding
+ *
+ */
+public class Test {
+	public static void main(String[] args) throws IOException {
+
+		HdfsMonitor
+				.init("D:/Program/workspace/stp-parent/stp-util/src/main/resources/hadoop.properties");
+
+		// DFSFileUtil.init("/var/lib/hadoop-hdfs/gelicollect/conf/hadoop.properties");
+
+		DistributedFileSystem hdfs = (DistributedFileSystem) HdfsMonitor.fs;
+
+		FsStatus ds = hdfs.getStatus();
+		long capacity = ds.getCapacity();
+		long used = ds.getUsed();
+		long remaining = ds.getRemaining();
+		long presentCapacity = used + remaining;
+		
+		DatanodeInfo[] live = hdfs.getDataNodeStats();
+		for (DatanodeInfo datanodeInfo : live) {
+			System.out.println(datanodeInfo.getDatanodeReport());
+		}
+		
+//		System.out.println(capacity);
+//		System.out.println(used);
+
+		// DatanodeInfo[] dataNodeStats = hdfs.getDataNodeStats();
+		// System.out.println("Configured Capacity     （单位字节）:"
+		// + hdfs.getRawCapacity());
+		// System.out.println("DFS Used     （单位字节）:" + hdfs.getRawUsed());
+		// System.out.println("DFS Remaining    （单位字节） :"
+		// + hdfs.getDiskStatus().getRemaining());
+		// System.out.println("DFS Used%      :更具上面的信息自己算百分比");
+		// System.out.println("DFS Remaining%     : 更具上面的信息自己算百分比");
+		// System.out.println("Node信息参考下方代码27行");
+		// System.out.println("Number of Under-Replicated Blocks     :"
+		// + hdfs.getUnderReplicatedBlocksCount());
+		//
+		// System.out.println("===========1111111111");
+		//
+		//
+		// for (DatanodeInfo datanodeInfo : dataNodeStats) {
+		// System.out.println(datanodeInfo.getName());
+		// System.out.println(datanodeInfo.getAdminState());
+		// System.out.println(datanodeInfo.getCapacity());
+		// System.out.println(datanodeInfo.getDfsUsed());
+		// System.out.println(datanodeInfo.getNonDfsUsed());
+		// System.out.println(datanodeInfo.getRemaining());
+		// System.out.println(datanodeInfo.getSoftwareVersion());
+		// System.out.println(datanodeInfo.getBlockPoolUsed());
+		//
+		// System.out.println("===========");
+		// }
+	}
+}
